0c9a6f3a9b31ade91cf23174f60a4f8e67f537f0
calebbabin <object.caleb@gmail.com>
2023-05-31 11:23:23 -0400

    add a wobbly circle

--- README.md
+++ README.md
@@ -1,13 +1,4 @@
-# 2D Canvas intro screen
-A starter template for 2d canvas based intro screens. [MDN has some great resources for learning about the canvas API.](https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API)
-
-> Confused? [Learn more about the project](https://github.com/moonscreens/info).
-
-If you want to make something with a little more GPU acceleration or 3D elements, check out [intro-example-threejs](https://github.com/moonscreens/intro-example-threejs)
-
-Include `?channel=moonmoon`, or `?channel=channel1,channel2,channel3...` in your URL to connect to a specific channel, great for testing!
-
-Include `?stats=true` to enable performance stats (should pop up in the top left corner).
+# Shapes - Passive animation
 
 # Development
 Before you start, you should have [NodeJS](https://nodejs.org/en/) installed.
@@ -23,5 +14,3 @@ After running `start`, open [localhost:5173](http://localhost:5173/). The page s
 npm run build
 ```
 Outputs static files to `/dist/`.
-
-Services like [Netlify](https://www.netlify.com/) have free tiers that can easily deploy simple static webpages like this.
--- src/main.js
+++ src/main.js
@@ -1,25 +1,7 @@
+import { createNoise3D } from 'simplex-noise';
 import './main.css';
-import Chat from 'twitch-chat-emotes';
 
-// a default array of twitch channels to join
-let channels = ['moonmoon'];
-
-// the following few lines of code will allow you to add ?channels=channel1,channel2,channel3 to the URL in order to override the default array of channels
-const query_vars = {};
-const query_parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (m, key, value) {
-	query_vars[key] = value;
-});
-
-if (query_vars.channels || query_vars.channel) {
-	const temp = query_vars.channels || query_vars.channel;
-	channels = temp.split(',');
-}
-
-// create our chat instance
-const ChatInstance = new Chat({
-	channels,
-	duplicateEmoteLimit: 5,
-})
+const noise3D = createNoise3D();
 
 const canvas = document.createElement('canvas');
 document.body.appendChild(canvas);
@@ -27,8 +9,8 @@ const ctx = canvas.getContext('2d');
 
 
 function resize() {
-	canvas.width = window.innerWidth;
-	canvas.height = window.innerHeight;
+	canvas.width = window.innerWidth * window.devicePixelRatio;
+	canvas.height = window.innerHeight * window.devicePixelRatio;
 }
 resize();
 window.addEventListener('resize', resize);
@@ -45,39 +27,35 @@ function draw() {
 
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-	for (let o = emoteArray.length - 1; o >= 0; o--) {
-		const emoteGroup = emoteArray[o];
 
-		// Keep track of where we should be drawing the next emote per message
-		let xOffset = 0;
+	const cx = canvas.width / 2;
+	const cy = canvas.height / 2;
 
-		for (let i = 0; i < emoteGroup.emotes.length; i++) {
-			const emote = emoteGroup.emotes[i];
-			ctx.drawImage(emote.canvas, xOffset + emoteGroup.x, emoteGroup.y);
-			xOffset = emote.canvas.width;
-		}
+	ctx.strokeStyle = '#999';
+	ctx.lineWidth = 4;
+	ctx.beginPath();
+	const circleRadius = Math.min(cx, cy) * 0.1;
+	const circlePoints = 64;
+	const t = performance.now() * 0.0001;
+	for (let i = 0; i < circlePoints; i++) {
+		const angle = i / circlePoints * Math.PI * 2;
+		let x = cx + Math.cos(angle) * circleRadius;
+		let y = cy + Math.sin(angle) * circleRadius;
+		const radiusModifier = noise3D(x * 0.01 + t, y * 0.01 + t, t);
 
-		// Delete a group after 10 seconds
-		if (emoteGroup.spawn < Date.now() - 10000) {
-			emoteArray.splice(o, 1);
+		x += Math.cos(angle) * radiusModifier * circleRadius * 0.1;
+		y += Math.sin(angle) * radiusModifier * circleRadius * 0.1;
+
+		if (i === 0) {
+			ctx.moveTo(x, y);
+		} else {
+			ctx.lineTo(x, y);
 		}
 	}
+	ctx.closePath();
+	ctx.stroke();
+
 
 }
-
-// add a callback function for when a new message with emotes is sent
-const emoteArray = [];
-ChatInstance.on("emotes", (emotes) => {
-
-	//prevent lag caused by emote buildup when you tab out from the page for a while
-	if (performance.now() - lastFrame > 1000) return;
-
-	emoteArray.push({
-		emotes,
-		x: Math.floor(Math.random() * canvas.width),
-		y: Math.floor(Math.random() * canvas.height),
-		spawn: Date.now()
-	});
-})
 
 draw();
